5ad509146a0ca787eacec8246cef5936a0eb0ad5
Felix Hummel <felix@felixhummel.de>
2013-01-11 22:37:09 +0100

    set -e

--- vbox_headless.sh
+++ vbox_headless.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e  # exit on any error
 # short version of http://www.howtoforge.com/vboxheadless-running-virtual-machines-with-virtualbox-4.1-on-a-headless-ubuntu-12.04-server
 EXTENSION_PACK_URL='http://download.virtualbox.org/virtualbox/4.2.6/Oracle_VM_VirtualBox_Extension_Pack-4.2.6-82870.vbox-extpack'
 echo 'deb http://download.virtualbox.org/virtualbox/debian precise contrib' | sudo tee --append /etc/apt/sources.list
