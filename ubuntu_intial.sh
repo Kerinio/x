40f3cfce84993f34f06268bfea455c8faefa04a1
Felix Hummel <felix@felixhummel.de>
2013-02-04 20:54:12 +0100

    command line tools

---
+++ ubuntu_intial.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# install command line tools I need on any machine
+
+sudo apt-get update
+
+# looks creepy, but easier to add/remove packages
+echo 'vim
+bash-completion
+wajig
+python-optcomplete
+gpm
+htop
+curl
+iotop
+atool
+lftp
+inotify-tools
+dnsutils
+lsof
+whois
+jnettop
+rsync
+' | sudo xargs -P 1 apt-get --yes install 
+
