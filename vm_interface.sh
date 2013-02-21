2a7f7be686d7013889f7ee5ed771d2708f5aa54d
Felix Hummel <felix@felixhummel.de>
2013-02-21 18:17:08 +0100

    comments ftw

--- vm_interface.sh
+++ vm_interface.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This is for a VM for multiple parties and bridged networking.
+# On distribution it should fetch an IP address from the router.
+# Problem: When changing the MAC address, the interface name changes
+# which then is not configured for DHCP.
+# Solution: One "eth0" to rule them all.
+
+# WARNING! This only works for VMs with a single interface!
+
 find /etc/udev/rules.d/ -name "*-net.rules" -exec rm {} \;
 cat <<'EOF' > /etc/udev/rules.d/70-persistent-net.rules
 SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{dev_id}=="0x0", ATTR{type}=="1", KERNEL=="eth*", NAME="eth0"
