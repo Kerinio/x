c829c3e74ded4c8776846b36aadc1a9087605300
Felix Hummel <felix@felixhummel.de>
2015-06-16 16:29:56 +0200

    more think less push
    not depending on git was the motivation, right?

--- git_from_source.sh
+++ git_from_source.sh
@@ -3,9 +3,9 @@
 set -e  # exit on any error
 sudo apt-get --assume-yes build-dep git
 sudo apt-get --assume-yes install libssl-dev xmlto
-latest_git_version=$(curl -s http://git-scm.com/ | grep "class='version'" | perl -pe 's/.*?([0-9\.]+)<.*/$1/') && echo ${latest_git_version}
+latest_git_version=$(curl -s https://api.github.com/repos/git/git/tags | perl -ne 'print "$1\n" if /name": "(v.*)"/' | sort | tail -n1) && echo ${latest_git_version}
 cd /tmp
-wget -O git-${latest_git_version}.tar.gz https://github.com/git/git/tarball/v${latest_git_version}
+wget -O git-${latest_git_version}.tar.gz https://github.com/git/git/tarball/${latest_git_version}
 tar xzf git-${latest_git_version}.tar.gz
 cd git-git-*
 sudo make prefix=/usr/local install install-doc
